023a1b079ab28e7ec1e4d031d67accf29806d9e3
Daniel <danielpargas14@gmail.com>
2015-10-27 10:31:33 -0430

    Actualizado Obtener Noticias

--- websevice/ObtenerTodasNoticias.php
+++ websevice/ObtenerTodasNoticias.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
  * Script PHP para obtener toda la tabla de noticias en la base de datos
  * Recibe idnoticia
@@ -17,57 +16,63 @@ $error = TRUE;
 require_once "../utilidades/Validaciones.php";
 //Se comprueba si las Variables estan definidas, caso contrario se asigna NULL
 //Clase con los metodos Genericos para el CRUB
-    require_once '../modelo/Crud.php';
+require_once '../modelo/Crud.php';
 //Se instancia un objeto de tipo CRUD
-    $modelo = new Crud();
+$modelo = new Crud();
 //Se inicializan los atributos para el Select            
-    //Columnas que se van a obtener 
-    $modelo->setSelect("*");
-    //Tabla de donde se van a obtener los datos
-    $modelo->setFrom("noticias");
-    //Condicion que establece que para obtener el tipo usuario que corresponde a un tipo usuario
-    //$modelo->setCondition("idanimal = $idanimal");
+//Columnas que se van a obtener 
+$modelo->setSelect("*");
+//Tabla de donde se van a obtener los datos
+$modelo->setFrom("noticias");
+//Condicion que establece que para obtener el tipo usuario que corresponde a un tipo usuario
+//$modelo->setCondition("idanimal = $idanimal");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $idtiponoticia = (isset($_POST["idtiponoticia"])) ? $_POST["idtiponoticia"] : NULL;
+    $idusuario = (isset($_POST["idusuario"])) ? $_POST["idusuario"] : NULL;
+    $reciente = isset($_POST["recientes"]);            
+
+    if (!is_null($idtiponoticia) && !is_null($idusuario)) {
+        $modelo->setCondition("idtiponoticia = $idtiponoticia and idusuario = $idusuario");
+    } elseif (!is_null($idtiponoticia)) {
+        $modelo->setCondition("idtiponoticia = $idtiponoticia");
+    } elseif (!is_null($idusuario)) {
+        $modelo->setCondition("idusuario = $idusuario");
+    }
     
-   if($_SERVER["REQUEST_METHOD"] == "POST"){
-                        
-        $idtiponoticia = (isset($_POST["idtiponoticia"])) ?  $_POST["idtiponoticia"] : NULL;
-        $idusuario = (isset($_POST["idusuario"])) ?  $_POST["idusuario"] : NULL;                
-                                
-                if(!is_null($idtiponoticia) && !is_null($idusuario)){
-                    $modelo->setCondition("idtiponoticia = $idtiponoticia and  idusuario = $idusuario");
-                }elseif (!is_null($idtiponoticia)){
-                    $modelo->setCondition("idtiponoticia = $idtiponoticia");
-                }elseif(!is_null($idusuario)){
-                    $modelo->setCondition("idusuario = $idusuario");
-                }                        
+    if ($reciente) {
+        $condicion = $modelo->getCondition();        
+        $modelo->setCondition($condicion." ORDER BY fecha DESC LIMIT 1");        
+    }
+}
+
+$datos = array();
+
+if ($modelo->Read() && !empty($modelo->getRows())) {
+    $filas = $modelo->getRows();
+
+    //Tiene idnoticia, idtiponoticia, idusuario, fecha, hora, titulo, descripcion, resumen            
+    foreach ($filas as $valor) {
+
+        $dato = array();
+
+        //iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($input));
+
+        $dato["idnoticia"] = $valor["idnoticia"];
+        $dato["idtiponoticia"] = $valor["idtiponoticia"];
+        $dato["idusuario"] = $valor["idusuario"];
+        $dato["fecha"] = $valor["fecha"];
+        $dato["hora"] = $valor["hora"];
+        $dato["titulo"] = $valor["titulo"];
+        $dato["descripcion"] = $valor["descripcion"];
+        $dato["resumen"] = $valor["resumen"];
+
+        array_push($datos, $dato);
     }
 
-    $datos = array();
-    
-        if ($modelo->Read() && !empty($modelo->getRows())) {
-            $filas=$modelo->getRows();
-
-            //Tiene idnoticia, idtiponoticia, idusuario, fecha, hora, titulo, descripcion, resumen            
-            foreach($filas as $valor){
-                
-                $dato = array();
-                                
-                //iconv('UTF-8', 'UTF-8//IGNORE', utf8_encode($input));
-                
-                $dato["idnoticia"] = $valor["idnoticia"];
-                $dato["idtiponoticia"] = $valor["idtiponoticia"];
-                $dato["idusuario"] = $valor["idusuario"];
-                $dato["fecha"] = $valor["fecha"];
-                $dato["hora"] = $valor["hora"];
-                $dato["titulo"] = $valor["titulo"];
-                $dato["descripcion"] = $valor["descripcion"];
-                $dato["resumen"] = $valor["resumen"];
-                
-                array_push($datos, $dato);
-            }
-                
-                $error = FALSE;
-        }
+    $error = FALSE;
+}
 //Si existe un error el estatus es 0
 
 if ($error) {
@@ -77,11 +82,11 @@ if ($error) {
 //Caso contrario es 1    
     $mensaje["estatus"] = 1;
     $mensaje["mensaje"] = "Noticias Obtenidas con Exito";
-    
-    
+
+
     $noticias["items"] = array();
     $noticias["items"] = $datos;
-        
+
     echo json_encode($noticias);
 }
 //Se usa la funcion json_encode para obtener el formato json del array    
